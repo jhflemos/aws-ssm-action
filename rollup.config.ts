2267dff41c51a406a55c0d9d9724c42e8e6f1ef2
Joao Lemos <jhflemos@gmail.com>
2025-12-31 12:15:21 +0100

    Fixing tests

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,3 @@
-// See: https://rollupjs.org/introduction/
-
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
@@ -9,10 +7,15 @@ const config = {
   output: {
     esModule: true,
     file: 'dist/index.js',
-    format: 'es',
+    format: 'cjs', // CommonJS is safest for Actions
     sourcemap: true
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
+  external: [
+    '@actions/core',
+    '@actions/github',
+    '@aws-sdk/client-ssm' // ← exclude AWS SDK from bundle
+  ]
 }
 
 export default config
