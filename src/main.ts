af7682c3edcaa1fc7fcb3b6da17d0658129ceadf
Joao Lemos <jhflemos@gmail.com>
2025-12-31 10:25:38 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 /**
  * The main function for the action.
@@ -8,18 +8,16 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const ssmPath = core.getInput("ssm-path");
+    core.info(`SSM Path: ${ssmPath}`);
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Get the current time and set it as an output variable
+    //const time = new Date().toTimeString();
+    //core.setOutput("time", time);
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    core.info(`The event payload: ${payload}`);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
